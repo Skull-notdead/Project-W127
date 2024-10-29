01b81431c7ba02f895276283c2f11d736f9a78ce
Skull-notdead <155349733+Skull-notdead@users.noreply.github.com>
2024-10-29 11:55:30 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,43 +5,55 @@ import humidity2 from './assets/humidity.png';
 import wind from './assets/wind.png';
 
 const apiKey = 'd3f4e955a985d0e021a345bf9c724664';
-const city = 'Hyderabad';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-async function getWeatherData() {
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   try {
     const response = await axios.get(apiUrl);
     const weatherData = response.data;
-    const temperature = weatherData.main.temp - 273.15; 
+    const temperature = weatherData.main.temp - 273.15; // Kelvin to Celsius conversion
     const windspeed = weatherData.wind.speed;
     const humidity = weatherData.main.humidity;
-    return { temperature, windspeed, humidity }; 
+    return { temperature, windspeed, humidity };
   } catch (error) {
-    console.error('Error:', error);
+    
     return null;
   }
 }
 
 function Card() {
+  const [city, setCity] = useState('Hyderabad');
   const [weatherData, setWeatherData] = useState({ temperature: null, windspeed: null, humidity: null });
 
   useEffect(() => {
-    getWeatherData().then((data) => {
+    getWeatherData(city).then((data) => {
       if (data) {
         setWeatherData(data);
       }
     });
-  }, []);
+  }, [city]);
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
 
   return (
     <div className='card'>
       <div>
-        <input className='input' type="text" placeholder="Select the city name" />
+        <input
+          className='input'
+          type="text"
+          placeholder="Select the city name"
+          value={city}
+          onChange={handleCityChange}
+        />
       </div>
       <div className='weather'>
         <img src={Hehe} className='weaicon' alt="weather icon" />
         <h1 className='h1'>{weatherData.temperature !== null ? `${weatherData.temperature.toFixed(2)} °C` : 'Loading...'}</h1>
+        
         <h2 className='city'>{city}</h2>
+        <div></div>
         <div className="details">
           <div className="col">
             <img src={humidity2} alt="humidity icon" className='lol' />
@@ -50,11 +62,11 @@ function Card() {
           </div>
           <div className='col'>
             <img src={wind} alt="wind icon" className='hehe' />
-            <p>speed{weatherData.windspeed !== null ? `${weatherData.windspeed} km/h` : 'Loading...'}</p>
-            
+            <p>{weatherData.windspeed !== null ? `${weatherData.windspeed} km/h` : 'Loading...'}</p>
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
